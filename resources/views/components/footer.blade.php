7201b1d9f14810043969b4e33847996c2374d630
zaiimrq <zaiimrq@gmail.com>
2024-11-10 16:26:11 +0900

    fix footer flex

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 @props(['show' => true])
 
 @if ($show)
-    <footer class="flex flex-wrap items-center justify-center mt-10 text-sm md:gap-2 md:justify-between md:text-md">
+    <footer class="flex flex-col items-center justify-center mt-10 text-sm md:gap-2 md:justify-between md:text-md sm:flex-row">
         <div class="flex items-center gap-2">
             <span>&copy; 2024</span>
             <span class="font-bold">zaiimrq</span>
