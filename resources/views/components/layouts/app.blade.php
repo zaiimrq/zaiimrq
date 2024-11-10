9669b34e6e84cb92c396ec229a72f70dee651fc5
zaiimrq <zaiimrq@gmail.com>
2024-11-11 08:50:56 +0900

    meta seo and google analytics

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,13 +1,36 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="black" class="dark" >
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="black" class="dark">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="keywords" content="zaiimrq, zaii, portfolio, pemrograman, laravel, php, belajar, web development, vb net, visual basic, programmer" >
+    <meta name="author" content="zaiimrq" >
+    <meta name="description" content="zaiimrq adalah situs web milik Slamet Shariful Zaidin. Lihat semua projek zaiimrq di bidang pemrograman." >
+    <meta property="og:title" content="Welcome - zaiimrq" >
+    <meta property="og:url" content="{{ url()->current() }}" >
+    <meta property="og:description" content="Lihat semua projek zaiimrq di bidang pemrograman." >
     <meta name="google-site-verification" content="tRwmooc9s6Vg7araHMQW1mtpYN9DnmpUdMGc_tJn5Fo" />
-    <title>{{ isset($title) ? $title . ' - ' . config('app.name') : config('app.name') }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{ Vite::asset('resources/img/profile.jpg') }}" type="image/x-icon">
+    <link rel="canonical" href="https://zaiimrq.serv00.net">
+    
+    <title>{{ isset($title) ? $title . ' - ' . config('app.name') : config('app.name') }}</title>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y1STXDFQ79"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-Y1STXDFQ79');
+    </script>
+    <!-- End of google tag analytics -->
+
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
